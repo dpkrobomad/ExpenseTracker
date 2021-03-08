f89d085683e44ba3aaa18293b97f3dfb6e6e7b92
deepak Radhakrishnan <dpkrobomad@gmail.com>
2021-03-09 00:23:06 +0530

    Added model and data

--- ExpTrack/app/admin.py
+++ ExpTrack/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Expense
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display =['Titles','Amount','Date']
 
 # Register your models here.
+admin.site.register(Expense,ExpenseAdmin)
--- ExpTrack/app/models.py
+++ ExpTrack/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Expense(models.Model):
+    Titles = models.CharField(max_length=200)
+    Amount = models.FloatField()
+    Date = models.DateTimeField()
+    Remarks = models.TextField(blank=True)
+    def __str__(self):
+        return self.Titles
